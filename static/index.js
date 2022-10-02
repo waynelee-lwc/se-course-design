00eda1b12d8bb3417f82e6fc8f7b191c3c2bccf2
waynelee-lwc <1418551940@qq.com>
2022-10-02 21:10:59 +0800

    [FE]index page

---
+++ static/index.js
@@ -0,0 +1,45 @@
+document.getElementsByTagName('html')[0].style.fontSize = (16/1920) * window.innerWidth + "px";
+window.onresize = function(){
+	// console.log("当前尺寸为：" + window.innerWidth);
+	document.getElementsByTagName('html')[0].style.fontSize = (16/1920) * window.innerWidth + "px";
+}
+
+let address = 'http://43.138.86.233:3011'
+
+$('.role-radio').click(function(e){
+    console.log(e)
+    console.log($(e).attr('id'))
+})
+
+$('.login-submit').on('click',function(){
+    let userid = $('.ID').val()
+    let password = $('.password').val()
+    let role = $("input:radio:checked").val()
+
+    $.ajax({
+        url:`${address}/user/login`,
+        type:'post',
+        data:{
+            id:userid,
+            password:password,
+            role:role
+        },
+        success:function(res){
+            if(res.code != 200){
+                alert(res.message)
+            }else{
+                localStorage.setItem('userinfo',JSON.stringify(res.data.user))
+                localStorage.setItem('token',JSON.stringify(res.data.token))
+                if(role == 'student'){
+                    location.href = '/student.html'
+                }
+                if(role == 'teacher'){
+                    location.href = '/teacher.html'
+                }
+                if(role == 'admin'){
+                    location.href = '/admin.html'
+                }
+            }
+        }
+    })
+})
